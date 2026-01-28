684acb5a20e4c158c368d5d9d761971988c4fbdf
anastudler <anistudler@gmail.com>
2026-01-28 16:27:13 -0300

    added tasks to database and rendered them in the UI.

--- src/renderer.js
+++ src/renderer.js
@@ -1,33 +1,25 @@
-/**
- * This file will automatically be loaded by vite and run in the "renderer" context.
- * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
- *
- * https://electronjs.org/docs/tutorial/process-model
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
- */
-
 import './index.css';
 
-console.log(
-  '👋 This message is being logged by "renderer.js", included via Vite',
-);
+const taskInput = document.getElementById('task-input');
+const addTaskBtn = document.getElementById('add-task-btn');
+const taskList = document.getElementById('task-list');
+
+const handleAddTask = async () => {
+    const title = taskInput.value.trim();
+    console.log(title);
+    await window.api.addTask(title);
+}
+
+addTaskBtn.addEventListener('click', handleAddTask);
+
+const renderTasks = async (params) => {
+    const tasks = await window.api.getAllTasks();
+
+    tasks.forEach(task => {
+        const li = document.createElement('li');
+        li.textContent = task.title;
+        taskList.appendChild(li);
+    })
+};
+
+renderTasks();
